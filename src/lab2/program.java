7624e905d23adadca21bc143614d338d50977b5d
THIEN_TAN <nguyenthientan2704@gmail.com>
2025-03-03 12:54:35 +0700

    Lab_2

---
+++ src/lab2/program.java
@@ -0,0 +1,10 @@
+package lab2;
+
+import lab2.excercise.Bai1;
+
+public class program {
+    public static void main(String[] args) {
+        Bai1 bai1 = new Bai1();
+        bai1.execBai1();
+    }
+}
